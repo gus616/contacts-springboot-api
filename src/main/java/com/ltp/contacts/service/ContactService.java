c21b07d184eb195462366dba81b021f0266ee4a3
Gustavo Velazquez <gustavo.velazquez.calderon.contractor@bbva.com>
2022-09-01 12:50:41 -0500

    primer commit

---
+++ src/main/java/com/ltp/contacts/service/ContactService.java
@@ -0,0 +1,11 @@
+package com.ltp.contacts.service;
+
+import com.ltp.contacts.pojo.Contact;
+
+public interface ContactService {
+    Contact getContactById(String id);
+    void saveContact(Contact contact);
+    void updateContact(String id, Contact contact);
+}
+
+
